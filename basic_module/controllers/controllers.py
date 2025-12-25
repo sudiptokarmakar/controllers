2f9ddcc3cd2408ecb95c96d15f426899db827513
sudiptokarmakar <iamroni1947@gmail.com>
2025-12-25 19:51:48 +0530

    second

--- basic_module/controllers/controllers.py
+++ basic_module/controllers/controllers.py
@@ -1,10 +1,10 @@
-# from odoo import http
+from odoo import http
 
 
-# class BasicModule(http.Controller):
-#     @http.route('/basic_module/basic_module', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class BasicModule(http.Controller):
+    @http.route('/hi', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
 #     @http.route('/basic_module/basic_module/objects', auth='public')
 #     def list(self, **kw):
